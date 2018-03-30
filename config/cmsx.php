e0042c81940708e78031cc7d6969a847ff4fcfc0
Michal Nosavcov <mnosavcov@gmail.com>
2018-03-30 20:27:41 +0200

    update

--- config/cmsx.php
+++ config/cmsx.php
@@ -6,6 +6,6 @@ return [
         'name' => 'Programování webu',
         'template' => 'cmsx-templates-default',
         'template-admin' => 'cmsx-templates-admin-default',
-        'template-custom' => 'vendor.cmsx.templates.default-custom.'
+        'template-custom' => 'vendor.cmsx.templates.default.customizable.'
     ]
 ];
