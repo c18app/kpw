d665c9e04cd646aa556a1b1370bb1a3ab9c72c95
Michal Nosavcov <mnosavcov@gmail.com>
2018-03-30 19:54:35 +0200

    update

--- config/cmsx.php
+++ config/cmsx.php
@@ -3,6 +3,9 @@
 return [
     'table_prefix' => 'c18x_',
     'app' => [
-        'name' => 'KPW (Kurz ProgramovÃ¡nÃ­ Webu)'
+        'name' => 'Programování webu',
+        'template' => 'cmsx-templates-default',
+        'template-admin' => 'cmsx-templates-admin-default',
+        'template-custom' => 'vendor.cmsx.templates.default-custom.'
     ]
 ];
