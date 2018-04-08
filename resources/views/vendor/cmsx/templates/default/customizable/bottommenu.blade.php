6581bbb1674760354fc9ba3f7cfd23299b67eded
Michal Nosavcov <mnosavcov@gmail.com>
2018-04-08 16:29:43 +0200

    patička

--- resources/views/vendor/cmsx/templates/default/customizable/bottommenu.blade.php
+++ resources/views/vendor/cmsx/templates/default/customizable/bottommenu.blade.php
@@ -2,6 +2,6 @@
     <div class="container">
         <div class="col-xs-6">Provozovatel webu: Michal Nosavcov, Nad Babím dolem 401, 250 64 Měšice<br>
             tel: 603&nbsp;323&nbsp;878, email: <a href="mailto:michal@programovaniwebu.cz">michal@programovaniwebu.cz</a>, IČ: 64845915 | <a href="{{ \C18app\Cmsx\Models\Page::findOrFail(18)->getUrl() }}" class="white">Ochrana osobních údajů</a></div>
-        <div class="col-xs-6 text-right white fb">Další tipy také<br>na facebooku</div>
+        <div class="col-xs-6 text-right"><a href="https://www.facebook.com/kurzprogramovaniwebu/" class="white fb" target="_blank">Další tipy také<br>na facebooku</a></div>
     </div>
 </div>
