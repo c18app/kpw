1aeddda6ae88cb1370f632bf9f98bb543ee63f04
Michal Nosavcov <mnosavcov@gmail.com>
2018-04-25 12:06:52 +0200

    odkazy

--- resources/views/vendor/cmsx/templates/default/customizable/homepage.blade.php
+++ resources/views/vendor/cmsx/templates/default/customizable/homepage.blade.php
@@ -10,11 +10,13 @@
     <ul class="articles">
         @foreach($clanky as $clanek)
             <li class="row">
+                <a href="{{ $clanek->getUrl() }}">
                 @if(file_exists(public_path('img/article/'.$clanek->id.'.png')))
                     <img src="{{ url('img/article/'.$clanek->id.'.png') }}" class="col-sm-4 hidden-xs">
                 @else
                     <img src="{{ url('img/pic01.png') }}" class="col-sm-4 hidden-xs">
                 @endif
+                </a>
                 <div class="box col-sm-8">
                     <h2><a href="{{ $clanek->getUrl() }}">{{ $clanek->title }}</a></h2>
                     <span>{{ substr(strip_tags($clanek->content), 0, 250) }}</span>
