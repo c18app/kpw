ff6beb07090a6daed4eb00aeb4029750e9c2ecca
Michal Nosavcov <mnosavcov@gmail.com>
2018-09-22 22:15:13 +0200

    dokumentace

--- resources/views/dokumentace-ke-kurzu/index.blade.php
+++ resources/views/dokumentace-ke-kurzu/index.blade.php
@@ -19,40 +19,54 @@
             background-image: none;
             padding-left: 15px;
         }
-    </style>
+
+        #page #main-content .content .panel {
+            box-shadow: none;
+            -webkit-box-shadow: none;
+            margin-bottom: 0;
+        }
+
+        #page #main-content .content pre {
+         padding: 10.5px
+        }
+</style>
     <div class="row">
         <ul class="col-xs-2 nav nav-pills nav-stacked">
             <li><a href="#html" data-toggle="collapse" data-parent="#accordion">HTML</a></li>
             <li><a href="#css" data-toggle="collapse" data-parent="#accordion">CSS</a></li>
             <li><a href="#css" data-toggle="collapse" data-parent="#accordion">javaScript</a></li>
-            <li><a href="#php">PHP</a></li>
-            <li><a href="#mysql">MySQL</a></li>
+            <li><a href="#php" data-toggle="collapse" data-parent="#accordion">PHP</a></li>
+            <li><a href="#mysql" data-toggle="collapse" data-parent="#accordion">MySQL</a></li>
             <li>
                 <hr>
             </li>
-            <li><a href="#nastroje">Nástroje</a></li>
+            <li><a href="#nastroje" data-toggle="collapse" data-parent="#accordion">Nástroje</a></li>
             <li>
                 <hr>
             </li>
-            <li><a href="#domena">Doména</a></li>
-            <li><a href="#server">Server</a></li>
-            <li><a href="#dns">DNS</a></li>
+            <li><a href="#domena" data-toggle="collapse" data-parent="#accordion">Doména</a></li>
+            <li><a href="#server" data-toggle="collapse" data-parent="#accordion">Server</a></li>
+            <li><a href="#dns" data-toggle="collapse" data-parent="#accordion">DNS</a></li>
+            <li><a href="#ssl" data-toggle="collapse" data-parent="#accordion">SSL</a></li>
             <li>
                 <hr>
             </li>
-            <li><a href="#fw">FW</a></li>
-            <li><a href="#git">GIT</a></li>
-            <li><a href="#composer">Composer</a></li>
+            <li><a href="#fw" data-toggle="collapse" data-parent="#accordion">FW</a></li>
+            <li><a href="#git" data-toggle="collapse" data-parent="#accordion">GIT</a></li>
+            <li><a href="#composer" data-toggle="collapse" data-parent="#accordion">Composer</a></li>
             <li>
                 <hr>
             </li>
-            <li><a href="#fw">Vzorový kód</a></li>
+            <li><a href="/kurzy/vzorovy/kod">Vzorový kód</a></li>
         </ul>
         <div class="col-xs-10">
             <div id="accordion">
                 <div class="panel">
                     <div id="dokumenty" class="collapse in">
-                        dokumenty
+                        <h2>Dokumentace</h2>
+                        <ul>
+                            <li><a href="#html" data-toggle="collapse" data-parent="#accordion">HTML</a></li>
+                        </ul>
                     </div>
                 </div>
                 <div class="panel">
