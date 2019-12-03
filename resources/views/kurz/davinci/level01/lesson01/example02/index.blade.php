7b72ba79d0e0e805cc0498237dfeb46e56fa6f72
Michal Nosavcov <mnosavcov@gmail.com>
2019-12-03 20:07:06 +0100

    d1

--- resources/views/kurz/davinci/level01/lesson01/example02/index.blade.php
+++ resources/views/kurz/davinci/level01/lesson01/example02/index.blade.php
@@ -229,8 +229,10 @@
             line-height: 1.5;
         }
     </style>
-    <p class="font-pacifico" style="margin-top: 50px;">Text s google fontem "Pacifico" <a
-                href="https://fonts.google.com/" target="_blank">https://fonts.google.com/</a></p>
+    <p class="font-pacifico" style="margin-top: 50px;">
+        Text s google fontem "Pacifico"
+        <a href="https://fonts.google.com/" target="_blank">https://fonts.google.com/</a>
+    </p>
 
     <div class="podtrzeni-odkazu">
         <span>podtržení odkazu</span>
@@ -279,8 +281,263 @@
 a.odkaz-po-najeti-zmeni-barvu:hover {
   color: #192243;
 }
-</style>')
-    }}</pre>
+
+img.kocicka {
+  background-color: #eee;
+  padding: 10px;
+  border: 1px solid #333;
+  box-shadow: #192243 5px 2px 8px 0px;
+  /* box-shadow: <barva> <vodorovný posun> <svislý posun> <rozostření> <rozprostření> */
+}
+
+.text-se-stinem {
+  font-size: 32px;
+  font-weight: bold;
+  color: #ee4445;
+  text-shadow: #192243 5px 2px 8px;
+  /* box-shadow: <barva> <vodorovný posun> <svislý posun> <rozostření> <rozprostření> */
+}
+
+.podtrzeni-odkazu {
+  margin-top: 50px;
+  width: 50%;
+  text-align: center;
+  cursor: pointer;
+}
+
+.podtrzeni-odkazu span {
+  font-size: 36px;
+  transition: all 0.2s ease-in-out;
+  position: relative;
+}
+
+.podtrzeni-odkazu span:before,
+.podtrzeni-odkazu span:after {
+  content: "";
+  position: absolute;
+  bottom: -5px;
+  width: 0px;
+  height: 5px;
+  margin: 5px 0 0;
+  transition: all 0.2s ease-in-out;
+  transition-duration: 0.75s;
+  opacity: 0;
+  background-color: #ee4445;
+}
+
+.podtrzeni-odkazu span:before {
+  left: 50%;
+}
+
+.podtrzeni-odkazu span:after {
+  right: 50%;
+}
+
+.podtrzeni-odkazu:hover span:before,
+.podtrzeni-odkazu:hover span:after {
+  opacity: 1;
+  width: 50%;
+}
+
+.zakladni-box {
+  margin-top: 50px;
+  width: 50%;
+  height: 100px;
+  background-color: #192243;
+  color: #fff;
+  text-align: center;
+  line-height: 100px;
+}
+
+.plynula-zmena-barvy {
+  transition: background-color .4s ease-out;
+}
+
+.plynula-zmena-barvy:hover {
+  background-color: #ee4445;
+}
+
+.presunuti-prvku-po-najeti {
+  position: relative;
+}
+
+.presunuti-prvku-po-najeti .kosticka {
+  position: absolute;
+  width: 25%;
+  height: 100px;
+  top: 0;
+  right: 0;
+  display: inline-block;
+  background-color: #ee4445;
+  transition: right 1s ease-out, width 1s ease-out;
+}
+
+.presunuti-prvku-po-najeti .text {
+  position: relative;
+  z-index: 1;
+}
+
+.presunuti-prvku-po-najeti:hover .kosticka {
+  width: 50%;
+  right: 50%;
+}
+
+.otoceny-prvek {
+  margin-top: 100px;
+  margin-bottom: 100px;
+  transform: rotate(160deg);
+}
+
+.duhove-pozadi {
+  color: #000;
+  font-weight: bold;
+  background-image: linear-gradient(to left, #ff0000, #ff7f00, #ffff00, #00ff00, #0000ff, #2e2b5f, #8b00ff)
+}
+
+.zvetseni-po-najeti {
+  transition: transform .4s ease-out;
+}
+
+.zvetseni-po-najeti:hover {
+  transform: scale(1.1);
+}
+
+.vice-efektu-po-najeti {
+  transition: transform .6s .4s ease-out, background-color .6s .4s ease-out, color .6s ease-out;
+}
+
+.vice-efektu-po-najeti:hover {
+  transform: scale(1.1) rotate(10deg);
+  background-color: #ee4445;
+  color: #192243;
+  transition: transform .6s ease-out, background-color .6s ease-out, color .6s .4s ease-out;
+}
+
+.animovany-prvek-1 {
+  position: relative;
+}
+
+.animovany-prvek-1 .kosticka {
+  display: inline-block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 25px;
+  height: 25px;
+  background-color: #ee4445;
+  animation-name: animace-1;
+  animation-duration: 5s;
+  animation-timing-function: linear;
+  animation-delay: 0s;
+  animation-iteration-count: infinite;
+  animation-direction: normal;
+  animation-fill-mode: forwards;
+}
+
+@keyframes animace-1 {
+  0% {
+    left: 0;
+    top: 0;
+  }
+  40% {
+    left: calc(100% - 25px);
+    top: 0;
+  }
+  50% {
+    left: calc(100% - 25px);
+    top: 75px;
+  }
+  90% {
+    left: 0;
+    top: 75px;
+  }
+}
+
+.animovany-prvek-2 {
+  display: inline-block;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  border: 20px solid transparent;
+  border-color: #ee4445 #192243 #ee4445 #192243;
+  animation: animace-2 2s linear infinite;
+  position: relative;
+  left: 0;
+}
+
+@keyframes animace-2 {
+  0% {
+    transform: rotate(0deg);
+    left: 0;
+  }
+  50% {
+    left: calc(50% - 100px);
+    transform: rotate(360deg);
+  }
+}
+</style>
+
+
+<p>
+  <a href="#" class="odkaz-po-najeti-zmeni-barvu" onclick="return false">odkaz po najetí změní barvu</a>
+</p>
+
+<p>
+  obrázek v rámečku se stínem<br>
+  <img src="https://blog.inpage.cz/obrazek/3/kitten-jpg/" style="width: 50%;" class="kocicka">
+</p>
+
+<p class="text-se-stinem">Text se stínem</p>
+
+<link href="https://fonts.googleapis.com/css?family=Pacifico&display=swap" rel="stylesheet">
+<style>
+  .font-pacifico {
+    font-family: \'Pacifico\', cursive;
+    font-size: 24px;
+    line-height: 1.5;
+  }
+</style>
+<p class="font-pacifico" style="margin-top: 50px;">
+  Text s google fontem "Pacifico"
+  <a href="https://fonts.google.com/" target="_blank">https://fonts.google.com/</a>
+</p>
+
+<div class="podtrzeni-odkazu">
+  <span>podtržení odkazu</span>
+</div>
+
+<p class="zakladni-box plynula-zmena-barvy">
+  prvek s plynulou změnou barvy
+</p>
+
+<p class="zakladni-box presunuti-prvku-po-najeti">
+  <span class="kosticka"></span>
+  <span class="text">přesunutí prvku po najetí</span>
+</p>
+
+<p class="zakladni-box otoceny-prvek">
+  otočený prvek o 160°
+</p>
+
+<p class="zakladni-box duhove-pozadi">
+  duhové pozadí
+</p>
+
+<p class="zakladni-box zvetseni-po-najeti">
+  zvětšení prvku po najetí
+</p>
+
+<p class="zakladni-box vice-efektu-po-najeti">
+  více efektů po najetí
+</p>
+
+<p class="zakladni-box animovany-prvek-1">
+  <span class="kosticka"></span>
+  animovaný prvek
+</p>
+
+<div class="animovany-prvek-2"></div>')
+}}</pre>
 
     <a href="/kurz/davinci/e3b777/01/01" style="float: right;">zpět</a>
 @endsection
